49e67c597c460179e4102d76e9a956de440988cd
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-14 15:47:03 -0600

    Adding notes

--- R/week5.R
+++ R/week5.R
@@ -6,7 +6,7 @@ library(tidyverse)
 Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum","stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv("../data/Anotes.csv")
 Bdata_tbl <- read_delim("../data/Bparticipants.dat", col_names = c("casenum", "parnum","stimver", "datadate", paste0("q",1:10))) # Why deleting delim works? 
-Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
+Bnotes_tbl <- read_tsv("../data/Bnotes.txt") # read_delim works too
 
 # Data Cleaning
 Aclean_tbl <- Adata_tbl %>%
