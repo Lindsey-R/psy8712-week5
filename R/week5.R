9ab23df7f6011dcfd0643bcf6dfd97375d10338b
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-14 15:45:49 -0600

    Finished Part 2

--- R/week5.R
+++ R/week5.R
@@ -8,6 +8,19 @@ Anotes_tbl <- read_csv("../data/Anotes.csv")
 Bdata_tbl <- read_delim("../data/Bparticipants.dat", col_names = c("casenum", "parnum","stimver", "datadate", paste0("q",1:10)))
 Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
 
-
+# Data Cleaning
+Aclean_tbl <- Adata_tbl %>%
+  separate_wider_delim(qs, delim = "-", names = paste0("q", 1:5)) %>%
+  mutate(datadate = mdy_hms(datadate)) %>%
+  mutate(across(contains("q"), ~ as.integer(.))) %>%
+  left_join(Anotes_tbl, by = "parnum") %>%
+  filter(is.na(notes))
+ABclean_tbl <- Bdata_tbl %>% 
+  mutate(datadate = mdy_hms(datadate)) %>%
+  mutate(across(contains("q"), ~ as.integer(.))) %>%
+  left_join(Bnotes_tbl, by = "parnum") %>%
+  filter(is.na(notes)) %>%
+  bind_rows(Aclean_tbl, .id = "lab")  %>% #Stack two tables on top with an identifier column 
+  select(-notes)
   
   
